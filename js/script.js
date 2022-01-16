443c0beb403e7229ed64b6c34322517163b356e7
ellijahcuts <ellijahcuts@gmail.com>
2022-01-17 00:35:50 +0300

    The task was repeated after the teacher.
    Need to redo it again

--- js/script.js
+++ js/script.js
@@ -1,61 +1,88 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-// - 1 -
-const promoAdv = document.querySelectorAll('.promo__adv img'),
-    promoBg = document.querySelector('.promo__bg'),
-    promoGenre = promoBg.querySelector('.promo__genre'),
-    promoInt = document.querySelector('.promo__interactive'),
-    promoIntItem = document.querySelector('.promo__interactive-list')
-/*promoIntItem = promoInt.getElementsByClassName("promo__interactive-item")*/
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    const promoAdv = document.querySelectorAll('.promo__adv img'),
+        promoBg = document.querySelector('.promo__bg'),
+        promoGenre = promoBg.querySelector('.promo__genre'),
+        promoInt = document.querySelector('.promo__interactive'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        promoIntItem = promoInt.querySelector(".promo__interactive-item"),
+        formAdd = document.querySelector('form.add'),
+        inputFilm = formAdd.querySelector('.adding__input'),
+        checkBox = formAdd.querySelector('[type="checkbox"]')
 
 
-promoAdv.forEach(i => {
-    i.remove()
-})
-
-promoGenre.textContent = "Драма"
-
-promoBg.style.background = 'url("../img/bg.jpg")'
 
 
-console.log(promoIntItem)
-promoIntItem.innerHTML = ""
-movieDB.movies.forEach((film, number) => {
-        promoIntItem.innerHTML += `<li class="promo__interactive-item">№${number+1} - ${film}
+    const sortArr = (arr) => {
+        arr.sort()
+    }
+
+    const deleteAdv = (adv) => {
+        adv.forEach(i => {
+            i.remove()
+        })
+    }
+
+    const makeChanges = () => {
+        promoGenre.textContent = "Драма"
+
+        promoBg.style.background = 'url("../img/bg.jpg")'
+    }
+
+    const createMovieList = (films, parent) => {
+        parent.innerHTML = ""
+        sortArr(films)
+        films.forEach((film, number) => {
+                parent.innerHTML += `<li class="promo__interactive-item">№${number + 1} - ${film}
                             <div class="delete"></div>
                         </li>`
+            }
+        )
+        document.querySelectorAll('.delete').forEach((btn, n)=>{
+            btn.addEventListener('click',()=>{
+                btn.parentElement.remove()
+                movieDB.movies.splice(n,1)
+                createMovieList(films, parent)
+
+            })
+        })
     }
-)
 
-/* Мое решение 4 и 5
-movieDB.movies.sort()
-for (let n = 0; n < movieDB.movies.length; n++) {
-    promoIntItem[n].innerText = `№${n + 1}. ${movieDB.movies[n]}`
-}
-*/
+    formAdd.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        let newFilmName = inputFilm.value
+        const favoriteFilm = checkBox.checked
+
+        if (newFilmName) {
+            if(newFilmName.length >21){
+    newFilmName = `${newFilmName.substring(0,22)}...`
+            }
+
+            if(favoriteFilm === true){
+                console.log("Добавляем любимый Фильм")
+            }
+            movieDB.movies.push(newFilmName)
+            sortArr(movieDB.movies)
+            createMovieList(movieDB.movies, movieList)
+        }
+        formAdd.reset()
+
+    })
 
 
-
+    createMovieList(movieDB.movies, movieList)
+    deleteAdv(promoAdv)
+    makeChanges()
+})
---
+++ js/script1.js
@@ -0,0 +1,53 @@
+/* Задания на урок:
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+// - 1 -
+const promoAdv = document.querySelectorAll('.promo__adv img'),
+    promoBg = document.querySelector('.promo__bg'),
+    promoGenre = promoBg.querySelector('.promo__genre'),
+    promoInt = document.querySelector('.promo__interactive'),
+    promoIntItem = document.querySelector('.promo__interactive-list')
+/*promoIntItem = promoInt.getElementsByClassName("promo__interactive-item")*/
+
+
+promoAdv.forEach(i => {
+    i.remove()
+})
+
+promoGenre.textContent = "Драма"
+
+promoBg.style.background = 'url("../img/bg.jpg")'
+
+
+console.log(promoIntItem)
+promoIntItem.innerHTML = ""
+movieDB.movies.forEach((film, number) => {
+        promoIntItem.innerHTML += `<li class="promo__interactive-item">№${number+1} - ${film}
+                            <div class="delete"></div>
+                        </li>`
+    }
+)
+
+/* Мое решение 4 и 5
+movieDB.movies.sort()
+for (let n = 0; n < movieDB.movies.length; n++) {
+    promoIntItem[n].innerText = `№${n + 1}. ${movieDB.movies[n]}`
+}
+*/
